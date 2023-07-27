47881f57706f37637b481dc04394433142c014d7
DaminiKhatik <khatikdamini25@gmail.com>
2023-07-27 14:36:39 +0530

    project added Successfully

---
+++ webinar landing page/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Set webinar details dynamically
+  document.getElementById("date").textContent = "July 10, 2023";
+  document.getElementById("time").textContent = "10:00 AM - 12:00 PM";
+  document.getElementById("duration").textContent = "2 hours";
